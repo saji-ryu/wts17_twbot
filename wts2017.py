83b3f34a50421f0e9cbed7cec76e8c046fb6cf6f
saji-ryu <rsajiki@gmail.com>
2018-01-15 15:12:35 +0900

    変更

--- wts2017.py
+++ wts2017.py
@@ -75,14 +75,14 @@ for line in codecs.open('output.txt.chasen','r','utf-8'):
 
     line = line.rstrip('\r\n')
     if line == "EOS":
-        # tweet_score[count][2] = point
         point = 0
         count += 1
         print count
     else:
         lis = line.split("\t")
         if lis[2] in dict:
-            point += point_rule[dict[lis[2]]]
+            tweet_score[count][2] += point
+            # point += point_rule[dict[lis[2]]]
 
 tweet_score.sort(key=lambda x:x[2],reverse=True)
 print tweet_score
