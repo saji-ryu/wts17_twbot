92a888e78270c6796fd6034a0e9f67d3c6a21862
saji-ryu <rsajiki@gmail.com>
2018-01-15 23:42:25 +0900

    順位

--- wts2017.py
+++ wts2017.py
@@ -113,17 +113,20 @@ tweet_score.sort(key=lambda x:x[2],reverse=True)
 
 # ツイート文
 text = '今日の季節感あるtweetランキング!!\n'
+rank = 1
 
 # 上位３つのツイートを表示する
-for i in range(3):
+for i in tweet_score
     # みんな０点だったら
     if tweet_score[0][2] <= 0:
         text = 'みなさん全然季節感がありませんね.......'
         break
     # 順位とユーザー名、非公式引用リツイートの形で前半２０文字をリツイート
+    if rank > 3:
+        break
     elif tweet_score[i][2] > 0 and tweet_score[i][3] != 'wts2017_team13':
-        text += str(i+1) + '位は' + tweet_score[i][1].encode('utf8') + 'さんでした　' + 'RT @' + tweet_score[i][3].encode('utf8') + ': ' + tweet_score[i][4].encode('utf8') + '\n'
-
+        text += str(rank) + '位は' + tweet_score[i][1].encode('utf8') + 'さんでした　' + 'RT @' + tweet_score[i][3].encode('utf8') + ': ' + tweet_score[i][4].encode('utf8') + '\n'
+        rank += 1
 if text == '今日の季節感あるtweetランキング!!\n':
     text = 'みなさん全然季節感がありませんね.......'
 
