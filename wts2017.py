67dbd9b7466a7040a2eb0d426381bf4404e7b621
saji-ryu <rsajiki@gmail.com>
2018-01-15 22:38:22 +0900

    テスト

--- wts2017.py
+++ wts2017.py
@@ -82,7 +82,8 @@ for line in codecs.open('output.txt.chasen','r','utf-8'):
     if line == "EOS":
         if noun_count/line_count > 0.8:
             tweet_score[tweet_count][2] = 0
-        pre = []
+            print tweet_score[tweet_count][3]
+        pre.clear()
         point = 0
         line_count = 0
         noun_count = 0
