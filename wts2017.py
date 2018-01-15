417e0d5a37a9f39e14a0caf453199c64159fc442
saji-ryu <rsajiki@gmail.com>
2018-01-16 00:14:35 +0900

    微修正

--- wts2017.py
+++ wts2017.py
@@ -84,11 +84,10 @@ pre = []
 for line in codecs.open('output.txt.chasen','r','utf-8'):
     line = line.rstrip('\r\n')
     if line == "EOS":
+        # 名詞の割合が8割を超える=日本語としておかしい時
         if noun_count/line_count > 0.8:
+            # ０点
             tweet_score[tweet_count][2] = 0
-            # print tweet_score[tweet_count][3],tweet_score[tweet_count][4]
-        # print pre
-        print tweet_score[tweet_count][3],tweet_score[tweet_count][4]
         del pre[:]
         point = 0
         line_count = 0
@@ -97,7 +96,7 @@ for line in codecs.open('output.txt.chasen','r','utf-8'):
     else:
         line_count += 1
         lis = line.split("\t")
-        # 季語の辞書にあった場合
+        # 季語の辞書にあった場合かつその言葉がツイート内で初めての時
         if lis[2] in dict and lis[2] not in pre:
             # 現在のツイートに季語から判断した得点を追加
             tweet_score[tweet_count][2] += point_rule[dict[lis[2]]]
@@ -106,7 +105,6 @@ for line in codecs.open('output.txt.chasen','r','utf-8'):
         if re.search(ur'名詞',lis[3]):
             noun_count += 1
 
-print tweet_count
 
 # 点数順に並び替え
 tweet_score.sort(key=lambda x:x[2],reverse=True)
@@ -121,12 +119,14 @@ for i in range(len(tweet_score)):
     if tweet_score[0][2] <= 0:
         text = 'みなさん全然季節感がありませんね.......'
         break
-    # 順位とユーザー名、非公式引用リツイートの形で前半２０文字をリツイート
+    # 表示は３位まで
     if rank > 3:
         break
+    # 順位とユーザー名、非公式引用リツイートの形で前半２０文字をリツイート
     elif tweet_score[i][2] > 0 and tweet_score[i][3] != 'wts2017_team13':
         text += str(rank) + '位は' + tweet_score[i][1].encode('utf8') + 'さんでした　' + 'RT @' + tweet_score[i][3].encode('utf8') + ': ' + tweet_score[i][4].encode('utf8') + '\n'
         rank += 1
+# 該当者０の場合
 if text == '今日の季節感あるtweetランキング!!\n':
     text = 'みなさん全然季節感がありませんね.......'
 
@@ -134,7 +134,7 @@ if text == '今日の季節感あるtweetランキング!!\n':
 print text
 
 # 投稿
-# try:
-#     api.update_status(status=text)
-# except tweepy.TweepError as e:
-#     print e
+try:
+    api.update_status(status=text)
+except tweepy.TweepError as e:
+    print e
